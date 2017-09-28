8833360364342b5aebb64c6ebfff09bcc4ade089
iwtw <wwttww@qq.com>
2017-09-28 19:23:07 +0800

    modified:   bin/nohup.out 	modified:   bin/train.sh 	new file:   src/train.py 	deleted:    bin/train.py 	modified:   src/tf_tools (modified content)

--- bin/train.sh
+++ bin/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python -u train.py -inputdir ~/datasets/asian-webface-lcnn-40/train.tfrecord  -n_gpus 2 -epoch_size 750106 --batch_size 64 --log_step 10000 &
+nohup python -u ../src/train.py -inputdir ~/datasets/asian-webface-lcnn-40/train.tfrecord  -n_gpus 2 -epoch_size 750106 --batch_size 64 --log_step 10000 &
 
